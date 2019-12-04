7539b561ce9bf0186c70ea1c286414133ed4c0ff
Griffin Going <griffingoing@gmail.com>
2019-12-04 09:55:08 -0600

    Center GUI on screen
    Adds code to open GUI in center of screen

--- src/MapApp/Classes/Main.java
+++ src/MapApp/Classes/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
         GUI gui = new GUI();
         gui.setVisible(true);
+        gui.setLocationRelativeTo(null);
 
         //DrawPanel.runProg(JOptionPane.showInputDialog("Enter file path for image"));
 
