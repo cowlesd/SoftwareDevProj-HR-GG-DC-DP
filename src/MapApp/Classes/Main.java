22a932b3c8aba5e3b6f6cd5cafdafd9909739044
Gabriel <cowlesd@mail.gvsu.edu>
2019-12-05 16:03:56 -0500

    New Gui: SuperiorWindow

--- src/MapApp/Classes/Main.java
+++ src/MapApp/Classes/Main.java
@@ -12,8 +12,10 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        GUI gui = new GUI();
-        gui.setVisible(true);
+        //GUI gui = new GUI();
+        //gui.setVisible(true);
+        SuperiorWindow neew = new SuperiorWindow();
+        neew.setVisible(true);
 
         //DrawPanel.runProg(JOptionPane.showInputDialog("Enter file path for image"));
 
